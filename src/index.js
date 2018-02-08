7908ed89f26ee2c3ca9af29cebc1da86c490e861
donkeypagoda <christopherallenstewart@gmail.com>
2018-02-08 15:06:28 -0800

    next steps

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ class Square extends React.Component {
   // }
   render() {
     return (
-      <button className="square" onClick={() => this.props.onClick({value:'X'})}>
-        {this.props.value}
+      <button className="square" onClick={props.onClick}>
+        {props.value}
       </button>
     );
   }
@@ -23,13 +23,17 @@ class Board extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true,
     }
   }
   handleClick(i) {
     const squares = this.state.squares.slice()
-    squares[i] = 'X'
-    this.setState({squares: squares})
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    })
   }
   renderSquare(i) {
     return (
@@ -40,7 +44,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
