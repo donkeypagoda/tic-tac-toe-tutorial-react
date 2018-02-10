4e09ed99f291ae1d04cba9bcc24ea767a93ab859
donkeypagoda <christopherallenstewart@gmail.com>
2018-02-09 16:32:30 -0800

    working on rows & colums

--- src/index.js
+++ src/index.js
@@ -84,7 +84,7 @@ class Game extends React.Component {
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
-      const desc = move ? "Go to move #" + move : "Go to game start";
+      const desc = move ? "Go to move #" + move + step.squares : "Go to game start";
       return (
         <li key={move}>
           <button onClick={() => this.jumpTo(move)}>{desc}</button>
