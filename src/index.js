3c5821aacf6742777474ae4e3a301c062bf3e47a
donkeypagoda <christopherallenstewart@gmail.com>
2018-03-13 16:18:33 -0700

    got it

--- src/index.js
+++ src/index.js
@@ -31,26 +31,11 @@ class Board extends React.Component {
         count++
       }
       boardArr.push(<div key={i} className="board-row">{squares}</div>)
-      
+      squares = []
     }
     return (
       <div>
         {boardArr}
-        // <div className="board-row">
-        //   {this.renderSquare(0)}
-        //   {this.renderSquare(1)}
-        //   {this.renderSquare(2)}
-        // </div>
-        // <div className="board-row">
-        //   {this.renderSquare(3)}
-        //   {this.renderSquare(4)}
-        //   {this.renderSquare(5)}
-        // </div>
-        // <div className="board-row">
-        //   {this.renderSquare(6)}
-        //   {this.renderSquare(7)}
-        //   {this.renderSquare(8)}
-        // </div>
       </div>
     );
   }
